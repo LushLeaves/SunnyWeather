f6a4553879bf13f93031069f283390db25a339c2
LushLeaves <ys_ag131@foxmail.com>
2021-12-08 10:16:50 +0800

    全球城市搜索天气

--- app/src/main/java/com/ys/sunnyweather/base/Constants.kt
+++ app/src/main/java/com/ys/sunnyweather/base/Constants.kt
@@ -1,6 +1,6 @@
 package com.ys.sunnyweather.base
 
 object Constants {
-    const val TOKEN = ""
+    const val TOKEN = "jhNt35MPn87mpeJM"
     const val BASE_URL="https://api.caiyunapp.com/"
 }
--- app/src/main/java/com/ys/sunnyweather/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/ys/sunnyweather/ui/place/PlaceFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ys.sunnyweather.R
@@ -40,13 +42,25 @@ class PlaceFragment : Fragment() {
             val content = editable.toString()
             if (content.isNotEmpty()) {
                 viewModel.searchPlaces(content)
-            }else{
-                binding.recyclerView.visibility=View.GONE
-                binding.bgImg.visibility=View.GONE
+            } else {
+                binding.recyclerView.visibility = View.GONE
+                binding.bgImg.visibility = View.VISIBLE
                 viewModel.placeList.clear()
                 adapter.notifyDataSetChanged()
             }
         }
-        viewModel.placeLiveData
+        viewModel.placeLiveData.observe(viewLifecycleOwner, Observer { result ->
+            val place = result.getOrNull()
+            if (place != null) {
+                binding.recyclerView.visibility = View.VISIBLE
+                binding.bgImg.visibility = View.GONE
+                viewModel.placeList.clear()
+                viewModel.placeList.addAll(place)
+                adapter.notifyDataSetChanged()
+            } else {
+                Toast.makeText(activity, "未能查询到任何地点", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+        })
     }
 }
